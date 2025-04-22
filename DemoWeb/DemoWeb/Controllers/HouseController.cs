7e622a61e20d8b1b5d19290d66a2c46888f60e17
Tomorrow <k77uj8ts25@gmail.com>
2025-04-22 09:28:46 +0800

    Create house controller and DTO

---
+++ DemoWeb/DemoWeb/Controllers/HouseController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HouseController : ControllerBase
+    {        
+        // GET api/<HouseController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HouseController>
+        [HttpPost]
+        public void Create([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HouseController>/5
+        [HttpPut("{id}")]
+        public void Update(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HouseController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoWeb/DemoWeb/Models/HouseDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace DemoWeb.Models
+{
+    public class HouseDTO
+    {
+        /// <summary>
+        /// House ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 房間數量
+        /// </summary>
+        public int NumberOfRooms { get; set; }
+
+        /// <summary>
+        /// 坪數
+        /// </summary>
+        public int NumberOfUnits { get; set; }
+
+        /// <summary>
+        /// 房屋售價
+        /// </summary>
+        public int Price { get; set; }
+    }
+}
